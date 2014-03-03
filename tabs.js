7c02c0ca82dd3f9b413e8b95bc1d60480ce7cada
Gabe G'Sell <gabergg@gmail.com>
2014-03-03 00:07:14 -0800

    updates to tabs

--- tabs.js
+++ tabs.js
@@ -1,8 +1,11 @@
 $(document).ready( function(){
   $('#biography_tab').show();
+  $('.selected').css("opacity","1");
   $('a').on('click',function(){
+      $('.selected').css("opacity",".5");
       $('.selected').removeClass('selected');
       $(this).closest('li').addClass('selected');
+      $('.selected').css("opacity","1");
 
       $('.content').hide();
 
